ffabc2c17a1e075dc963e74601e0c51669b08eba
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-11 21:52:47 +0100

    chore: config eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "es2021": true,
+    "node": true,
+  },
+  "extends": "standard-with-typescript",
+  "overrides": [],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": __dirname,
+  },
+  "rules": {},
+  'ignorePatterns': [
+    '.eslintrc.js'
+  ],
+}
