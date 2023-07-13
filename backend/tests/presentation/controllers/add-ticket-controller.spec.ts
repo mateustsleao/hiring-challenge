9e4827db1d4762b91b6d2b84335828823612c021
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-13 01:07:38 +0100

    test: ensures that return 400 if AddTicket Validations fails

--- backend/tests/presentation/controllers/add-ticket-controller.spec.ts
+++ backend/tests/presentation/controllers/add-ticket-controller.spec.ts
@@ -1,6 +1,6 @@
 import { AddTicketController, type AddTicketControllerRequest } from '@/presentation/controllers'
 import { ValidationSpy, AddTicketSpy } from '@/tests/presentation/mocks'
-import { serverError } from '@/presentation/helpers'
+import { badRequest, serverError } from '@/presentation/helpers'
 import { faker } from '@faker-js/faker'
 
 import MockDate from 'mockdate'
@@ -53,6 +53,13 @@ describe('AddTicketController', () => {
     jest.spyOn(addTicketSpy, 'add').mockImplementationOnce(() => { throw new Error() })
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 400 if Validation fails', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 }
 )
