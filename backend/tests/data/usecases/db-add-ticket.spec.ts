25ed24758d770d3de73276fe1eb78b961c936344
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-13 19:45:25 +0100

    test: ensure AddTicketRespository called with correct values

---
+++ backend/tests/data/usecases/db-add-ticket.spec.ts
@@ -0,0 +1,36 @@
+import { AddTicketRepositorySpy } from '@/tests/data/mocks'
+import { DbAddTicket } from '@/data/usecases'
+import { mockAddTicketParams } from '@/tests/domain/mocks'
+
+import MockDate from 'mockdate'
+
+describe('DbAddTicket Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  const makeSut = (): SutTypes => {
+    const addTicketRepositorySpy = new AddTicketRepositorySpy()
+    const sut = new DbAddTicket(addTicketRepositorySpy)
+    return {
+      sut,
+      addTicketRepositorySpy
+    }
+  }
+
+  interface SutTypes {
+    sut: DbAddTicket
+    addTicketRepositorySpy: AddTicketRepositorySpy
+  }
+
+  test('Should call AddTicketRepository with correct values', async () => {
+    const { sut, addTicketRepositorySpy } = makeSut()
+    const ticketData = mockAddTicketParams()
+    await sut.add(ticketData)
+    expect(addTicketRepositorySpy.params).toEqual(ticketData)
+  })
+})
