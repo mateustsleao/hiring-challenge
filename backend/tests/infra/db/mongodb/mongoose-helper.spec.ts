c15cce4d2c0cd852dd947e94e44466ad0f2f38b6
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-15 15:31:15 +0100

    test: ensure mongodb reconnect if client is down

--- backend/tests/infra/db/mongodb/mongoose-helper.spec.ts
+++ backend/tests/infra/db/mongodb/mongoose-helper.spec.ts
@@ -5,37 +5,27 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 const makeDbStub = async (): Promise<string> => {
   const mongo = await MongoMemoryServer.create()
   return mongo.getUri()
+  //  mockAddTicketParams()
 }
 const uri = makeDbStub()
 
 describe('Mongoose Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(await uri)
+  })
+  afterAll(async () => {
+    await sut.disconnect()
+  })
   test('Should reconnect if mongodb is down', async () => {
     await sut.connect(await uri)
+    let ticketModel = sut.getModel('Ticket', await uri)
+    expect(ticketModel).toBeTruthy()
     expect(sut.client).toBeTruthy()
     await sut.disconnect()
     expect(sut.client).toBeFalsy()
     await sut.connect(await uri)
+    ticketModel = sut.getModel('Ticket', await uri)
+    expect(ticketModel).toBeTruthy()
     expect(sut.client).toBeTruthy()
   })
-  // test('Should map', async () => {
-  //   const result = sut.map(mockAddTicketParams())
-  //   expect(result).toEqual({
-  //     id: 'any_id',
-  //     title: 'any_title',
-  //     description: 'any_description',
-  //     body: 'any_body',
-  //     userId: 'any_userId',
-  //     createdAt: 'any_createdAt',
-  //     updatedAt: 'any_updatedAt'
-  //   })
-  // })
-  // test('Should return null if getModel fails', async () => {
-  //   const result = sut.getModel('any_name')
-  //   expect(result).toBeNull()
-  // })
-  // test('Should return a model if getModel succeeds', async () => {
-  //   await sut.connect(uri)
-  //   const result = sut.getModel('Ticket')
-  //   expect(result).toBeTruthy()
-  // })
 })
