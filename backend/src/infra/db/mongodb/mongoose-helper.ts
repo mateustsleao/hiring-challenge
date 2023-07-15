48240c1cc42f0706bae9a18aabe95f652a970e7e
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-15 02:57:31 +0100

    refactor: ensure db connection and desconnection

--- backend/src/infra/db/mongodb/mongoose-helper.ts
+++ backend/src/infra/db/mongodb/mongoose-helper.ts
@@ -1,13 +1,35 @@
 import mongoose from 'mongoose'
-export const MongooseHelper = {
+
+interface MongooseHelperType {
+  client: mongoose.Mongoose | null
+  connect: (uri: string, options?: mongoose.ConnectOptions | undefined) => Promise<void>
+  disconnect: () => Promise<void>
+  getModel: (name: string) => mongoose.Model<mongoose.Document> | null
+  map: (collection: any) => any
+}
+
+const mongooseHelper: MongooseHelperType = {
   client: mongoose,
-  async connect (uri: string, options?: mongoose.ConnectOptions | undefined): Promise<void> {
+  async connect (uri, options): Promise<void> {
+    if (this.client === null) {
+      this.client = mongoose
+    }
     await this.client.connect(uri, options)
   },
   async disconnect (): Promise<void> {
+    if (this.client === null) return
+
     await this.client.disconnect()
+    this.client = null
   },
-  getModel (name: string): mongoose.Model<mongoose.Document> {
+  getModel (name) {
+    if (this.client === null) return null
     return this.client.model(name)
+  },
+  map: (collection) => {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
   }
 }
+
+export const MongooseHelper = mongooseHelper
