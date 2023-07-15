84c0d0126b26f0b58c58d5369322f3206145d7fb
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-15 21:41:33 +0100

    feat: add map function

---
+++ backend/src/infra/db/mongoose/mongoose-helper.ts
@@ -0,0 +1,38 @@
+import mongoose, { type Model, type Document } from 'mongoose'
+
+const mongooseHelper = {
+  client: null as mongoose.Mongoose | null,
+  async connect (uri: string, options?: mongoose.ConnectOptions): Promise<void> {
+    if (this.client === null) {
+      this.client = mongoose
+    }
+    await this.client.connect(uri, options)
+  },
+  async disconnect (): Promise<void> {
+    if (this.client === null) return
+
+    await this.client.disconnect()
+    this.client = null
+  },
+  async getModel<T extends Document> (name: string, uri: string): Promise<Model<T> | undefined> {
+    if (this.client === null) {
+      await this.connect(uri)
+    }
+    const moongoseModel = this.client?.model<T>(name)
+    return moongoseModel ?? undefined
+  },
+  map (moongoseModel: any): any {
+    const model = moongoseModel.toObject({
+      versionKey: false,
+      transform: (doc: any, ret: any) => {
+        const { _id, ...docWithoutId } = ret
+        return Object.assign({}, docWithoutId, { id: _id.toString() })
+      }
+    })
+
+    console.log('modelitous', model)
+    return model
+  }
+}
+
+export const MongooseHelper = mongooseHelper
