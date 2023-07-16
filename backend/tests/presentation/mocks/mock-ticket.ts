469426d7fc46d590a163dea55338f34e6a6ec172
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-16 22:51:45 +0100

    refactor: add mockRequest and mockResponse on mock-ticket

--- backend/tests/presentation/mocks/mock-ticket.ts
+++ backend/tests/presentation/mocks/mock-ticket.ts
@@ -1,4 +1,6 @@
 import { type AddTicketParams, type AddTicket } from '@/domain/usecases'
+import { type HttpRequest, type HttpResponse } from '@/presentation/protocols'
+import { faker } from '@faker-js/faker'
 
 export class AddTicketSpy implements AddTicket {
   params: AddTicketParams | undefined
@@ -7,3 +9,23 @@ export class AddTicketSpy implements AddTicket {
     this.params = params
   }
 }
+
+export const mockRequest = (): HttpRequest => ({
+  body: {
+    client: faker.person.firstName(),
+    issue: faker.lorem.sentence(),
+    status: 'open',
+    deadline: faker.date.soon()
+  }
+})
+
+export const mockResponse = (): HttpResponse => ({
+  statusCode: 200,
+  body: {
+    client: faker.person.firstName(),
+    issue: faker.lorem.sentence(),
+    status: 'open',
+    deadline: faker.date.soon(),
+    id: faker.datatype.uuid()
+  }
+})
