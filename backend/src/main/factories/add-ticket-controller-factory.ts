5b3e7115ee873d934a5eb06ae92df10809816394
Mateus Toledo da Silveira Leão <89942457+mateustsleao@users.noreply.github.com>
2023-07-16 21:47:08 +0100

    feat: ensure calls LogController decorator with correct values

--- backend/src/main/factories/add-ticket-controller-factory.ts
+++ backend/src/main/factories/add-ticket-controller-factory.ts
@@ -1,8 +1,9 @@
 import { makeAddTicketValidation, makeDbAddTicket } from '@/main/factories'
 import { type Controller } from '@/presentation/protocols'
 import { AddTicketController } from '@/presentation/controllers'
+import { LogControllerDecorator } from '@/main/decorators'
 
 export const makeAddTicketController = (): Controller => {
   const controller = new AddTicketController(makeDbAddTicket(), makeAddTicketValidation())
-  return controller
+  return new LogControllerDecorator(controller)
 }
